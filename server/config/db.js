cf1db7e3a77b585791831abc3b1cf32f395bb28c
leslie-23 <leslieajayi27@gmail.com>
2025-02-11 20:41:11 +0000

    update v-01

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI, {
+    const connection = await mongoose.connect(process.env.MONGO_URI_TEST, {
       // useNewUrlParser: true,
       // useUnifiedTopology: true,
       // useCreateIndex: true,
