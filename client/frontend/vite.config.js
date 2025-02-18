dad545f906ff34e4225ea12d42311d5ac1fba78d
leslie-23 <leslieajayi27@gmail.com>
2025-02-18 09:08:44 +0000

    netlify issues

--- client/frontend/vite.config.js
+++ client/frontend/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TailwindCSSVitePlugin } from "tailwindcss-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    //  TailwindCSSVitePlugin(),
-  ],
+  plugins: [react()],
   base: "/",
   build: {
     outDir: "dist",
